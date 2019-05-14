10d3cbc8e0a3999a67539f5fb08ea6ef5fa47862
nbambach <nebambach@gmail.com>
2019-05-14 14:37:23 -0700

    Create rip760.py

---
+++ apps/rip760.py
@@ -0,0 +1,21 @@
+rip760_layout = html.Div([
+    html.Div([
+        html.H2("Ripperdan 760 | ET Tool",
+                style={'display': 'inline',
+                        'float': 'left',
+                       'font-size': '2.0 em',
+                       'margin-left': '8px',
+                       'font-weight': 'bolder',
+                       'font-family': 'Product Sans',
+                       'color': "rgba(117, 117, 117, 0.95)",
+                       'margin-top': '20px',
+                       'margin-bottom': '0',
+                       }),
+            ], className='banner'),
+    html.Div([
+        html.Div([
+            html.H3("")
+        ],style={'padding': '10px 10px 70px 10px'},
+            className='Title')
+    ])
+])
